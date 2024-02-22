9985da4bb123ea5d393d9eb6cf0f60785c6ab2c2
Artem4ik <artem4iksmirnov1984gmail.com>
2024-02-22 21:44:18 +0300

    PostOfTheObject

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
